dcc90e5c23067fe59c1fbce954b1aeb19b39361c
nrismawardah <nrismawardah@gmail.com>
2023-05-15 20:27:53 +0700

    Membuat kelas mahasiswa

---
+++ mbrStatikfunc/mbrStatikfunc.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
+	static int getNim() { return nim; /*Definisi Function*/ }
+
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Sri Dadi");
+	mahasiswa mhs2("Budi Jatmiko");
+	mahasiswa::setNim(9); // Mengakses nim melalui static member function "setNim"
+	mahasiswa mhs3("Andi Janu");
+	mahasiswa mhs4("Joko Wohono");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "Akses dari luar object = " << mahasiswa::getNim() << endl;	// Mengakses nim melalui static member function "getNim"
+
+	return 0;
+}
