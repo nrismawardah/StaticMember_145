7da301f8ade1aed0f8a30579f45624a16fde60c3
nrismawardah <nrismawardah@gmail.com>
2023-05-15 19:45:58 +0700

    Memanggil destruktor

--- Destruktor/Destruktor.cpp
+++ Destruktor/Destruktor.cpp
@@ -39,6 +39,7 @@ void angka::isiData() {
 }
 
 int main() {
-	angka belajarcpp(3);
-	angka* ptrBelajarcpp = new angka(5);
+	angka belajarcpp(3);					// Constructor dipanggil
+	angka* ptrBelajarcpp = new angka(5);	// Constructor dipanggil
+	delete ptrBelajarcpp;					// Destructor dipanggil
 }
