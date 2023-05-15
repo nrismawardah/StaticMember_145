64415c5dd1cbdd864294728256bdec9ced1ef11f
nrismawardah <nrismawardah@gmail.com>
2023-05-15 11:40:48 +0700

    Membuat metode PrintAll

--- StaticMember_145/StaticMember_145.cpp
+++ StaticMember_145/StaticMember_145.cpp
@@ -16,5 +16,11 @@ public:
 int mahasiswa::nim = 0;
 
 void mahasiswa::setID() {
+	id = ++nim;
+}
 
+void mahasiswa::printAll() {
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
 }
